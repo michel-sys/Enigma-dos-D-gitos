82fc6c502c359acf496b6244c1dee343eb01a0e9
MichelJJ <82042407+michel-sys@users.noreply.github.com>
2024-02-16 20:19:31 -0300

    2.1

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function chutar() {
     atualizarPontuacao();
     resetarJogo();
   } else {
-    let mensagem = chute < numero ? "O número é maior!" : "O número é menor!";
+    let mensagem = chute < numero ? "O número é maior que " + chute + '!' : "O número é menor que " + chute + "!";
     exibirMensagem(mensagem, "errado");
     if (tentativas == 3) {
       exibirMensagem(
